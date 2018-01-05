d71169dd34994cec9c79b34749e5f1692715fa22
Adaleigh Martin <adaleigh.martin@gmail.com>
2018-01-05 14:38:19 -0800

    Redo premake.lua

--- premake4.lua
+++ premake4.lua
@@ -1,35 +1,41 @@
 #!lua
 
-solution "TileEngine"
-  configurations { "Debug", "Release", "WindowsDebug" }
+solution "NymphGameEngine"
+  configurations { "Debug", "Release", "WindowsDebug", "WindowsRelease" }
 
-  project "TileEngine"
+  project "NymphGameEngine"
     kind "ConsoleApp"
     language "C++"
 
-    targetname "TileEngine"
+    targetname "NymphGameEngine"
 
     includedirs { "./include", "./src" }
 
     files { "**.h", "**.hpp", "**.cpp", "**.cc" }
-    excludes { "./test/**, ./deps/**" }
-
-    configuration "WindowsDebug"
-      buildoptions { "-std=c++11" }
-      links { "freetype_win", "yse", "glfw3", "gdi32", "glew32s", "opengl32", "png", "z", "DevIL", "chaiscript_stdlib-5.8.5" }
-      defines { "DEBUG", "GLEW_STATIC" }
-      targetextension ".exe"
+    excludes { "./test/**" }
 
     configuration "Debug"
-      buildoptions { "-std=c++14", "-Wno-unknown-pragmas", "-g", "-O0", '-pthread' }
-      links {  "freetype", "yse", "glfw", "png", "z", "OpenGL.framework", "glew", "IL" }
+      buildoptions { "-std=c++14",  "-g", "-O0", '-pthread' }
+      links {  "freetype", "yse", "glfw", "png", "z", "OpenGL.framework", "glew", "IL", "tmxparser", "chaiscript" }
       defines { "DEBUG" }
       flags { "Symbols" }
       targetextension ".out"
 
     configuration "Release"
-      buildoptions { "-std=c++11", "-Wno-unknown-pragmas", "-g", "-O0", '-pthread' }
-      links {  "freetype", "yse", "glfw3.3.1", "png", "z", "OpenGL.framework", "glew", "IL", "chaiscript_stdlib-5.8.5" }
+      buildoptions { "-std=c++14", "-pthread" }
+      links {  "freetype", "yse", "glfw", "png", "z", "OpenGL.framework", "glew", "IL", "tmxparser", "chaiscript" }
       defines { "NDEBUG" }
       flags { "Optimize" }
       targetextension ".out"
+
+    configuration "WindowsDebug"
+      buildoptions { "-std=c++14" }
+      links { "freetype_win", "yse", "glfw3", "gdi32", "glew32s", "opengl32", "png", "z", "DevIL", "chaiscript_stdlib-5.8.5" }
+      defines { "DEBUG", "GLEW_STATIC" }
+      targetextension ".exe"
+
+    configuration "WindowsRelease"
+      buildoptions { "-std=c++14" }
+      links { "freetype_win", "yse", "glfw3", "gdi32", "glew32s", "opengl32", "png", "z", "DevIL", "chaiscript_stdlib-5.8.5" }
+      defines { "DEBUG", "GLEW_STATIC" }
+      targetextension ".exe"
