938f3f398b935f62096d62d6fc3616d9c4262f50
Adaleigh Martin <adaleigh.martin@gmail.com>
2018-01-05 17:48:49 -0800

    Add dynamic loader, pwd to see around chaiscript make what is happening.

--- premake4.lua
+++ premake4.lua
@@ -30,14 +30,14 @@ solution "NymphGameEngine"
 
     configuration "LinuxDebug"
       buildoptions { "-stdlib=libstdc++", "-std=c++14", "-g", "-O0", '-pthread' }
-      links {  "freetype", "yse", "glfw", "png", "z", "GL", "glew", "IL", "tmxparser", "chaiscript_stdlib-6.0.0" }
+      links {  "freetype", "yse", "glfw", "png", "z", "GL", "glew", "IL", "tmxparser", "dl", "chaiscript_stdlib-6.0.0" }
       defines { "DEBUG" }
       flags { "Symbols" }
       targetextension ".out"
 
     configuration "LinuxRelease"
       buildoptions { "-stdlib=libstdc++", "-std=c++14", "-pthread" }
-      links {  "stdc++", "freetype", "yse", "glfw", "png", "z", "GL", "glew", "IL", "tmxparser", "chaiscript_stdlib-6.0.0" }
+      links {  "freetype", "yse", "glfw", "png", "z", "GL", "glew", "IL", "tmxparser", "dl", "chaiscript_stdlib-6.0.0" }
       defines { "NDEBUG" }
       flags { "Optimize" }
       targetextension ".out"
