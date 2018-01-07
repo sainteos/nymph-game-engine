67ea941aac3dab64e6e10f471ab3872b5e8c43bd
Adaleigh Martin <adaleigh.martin@gmail.com>
2018-01-06 17:09:22 -0800

    Change linker order

--- premake4.lua
+++ premake4.lua
@@ -30,14 +30,14 @@ solution "NymphGameEngine"
 
     configuration "LinuxDebug"
       buildoptions { "-stdlib=libc++", "-std=c++14", "-g", "-O0", '-pthread', "`freetype-config --cflags`" }
-      links {  "pthread", "freetype", "glfw", "png", "z", "GL", "GLEW", "IL", "tmxparser", "dl", "chaiscript_stdlib-6.0.0" }
+      links {  "freetype", "glfw", "png", "z", "GLEW", "IL", "tmxparser", "dl", "chaiscript_stdlib-6.0.0", "GL", "pthread" }
       defines { "DEBUG" }
       flags { "Symbols" }
       targetextension ".out"
 
     configuration "LinuxRelease"
       buildoptions { "-stdlib=libc++", "-std=c++14", "-pthread", "`freetype-config --cflags`" }
-      links {  "pthread", "freetype", "glfw", "png", "z", "GL", "GLEW",  "IL", "tmxparser", "dl", "chaiscript_stdlib-6.0.0" }
+      links {  "freetype", "glfw", "png", "z", "GLEW",  "IL", "tmxparser", "dl", "chaiscript_stdlib-6.0.0", "GL", "pthread" }
       defines { "NDEBUG" }
       flags { "Optimize" }
       targetextension ".out"
