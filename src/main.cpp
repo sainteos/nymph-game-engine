d84e88e0ec0dff4db9d1a2aff6bbe3515d28e320
Adaleigh Martin <adaleigh.martin@gmail.com>
2018-01-11 19:39:39 -0800

    Remove config from project, it will be supplied by project that uses engine. Make default load config to be nymph_config. Also can supply config by giving path as argument when running from shell.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <easylogging++.h>
 #include <sstream>
+#include <iostream>
 #include "engine.h"
 
 INITIALIZE_EASYLOGGINGPP
@@ -8,14 +9,23 @@ INITIALIZE_EASYLOGGINGPP
 
 int main(int argc, char** argv) {
   START_EASYLOGGINGPP(argc, argv);
-  
+
   LOG(INFO)<<"Number of args: "<<argc;
   std::string config_path;
   if(argc > 2) {
-    config_path = std::string(argv[1]);
+    if(argv[1] == "-h" || argv[1] == "--help") {
+      std::cout<<"Nymph Game Engine USAGE"<<std::endl;
+      std::cout<<"Running with no arguments: will attempt to load engine config from nymph_config."<<std::endl;
+      std::cout<<"NymphGameEngine ./path/to/config.json: attempts to load engine config from supplied path."<<std::endl;
+      std::cout<<"NymphGameEngine -h (or) --help: supplies this help menu."<<std::endl;
+      return 0;
+    }
+    else {
+      config_path = std::string(argv[1]);
+    }
   }
   else {
-    config_path = "./config/default.json";
+    config_path = "./nymph_config.json";
   }
 
   std::shared_ptr<Engine> engine = std::make_shared<Engine>();
