11223ed72cbbd34c81183c3ee993cb6b9b3c05cc
Adaleigh Martin <adaleigh.martin@gmail.com>
2018-01-11 19:25:40 -0800

    remove premake_plus.rb

--- premake_plus.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-`ruby script_registration_generator.rb`
-`premake4 gmake`
-makefile = File.read("TileEngine.make")
-makefile.gsub!("CC = gcc", "CC = $(CROSS)gcc")
-makefile.gsub!("CXX = g++", "CXX = $(CROSS)g++")
-makefile.gsub!("AR = ar", "AR = $(CROSS)ar")
-File.new("TileEngine.make", "w") << makefile
